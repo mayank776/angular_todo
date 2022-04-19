6564647d3096469617ae0715537ce2cb2aa4c5ce
Mayank Attri <attrimayank16@gmail.com>
2022-04-19 15:41:05 +0530

    final push

--- my-first-project/src/app/mycomponent/to-dos/to-dos.component.ts
+++ my-first-project/src/app/mycomponent/to-dos/to-dos.component.ts
@@ -9,27 +9,14 @@ import { Todo } from '../../Todo';
 export class ToDosComponent implements OnInit {
   todos: Todo[];
 
+  localItem: string;
+
   constructor() {
-    this.todos = [
-      {
-        sno: 3,
-        title: 'title',
-        desc: 'description',
-        active: true,
-      },
-      {
-        sno: 5,
-        title: 'second title',
-        desc: 'description again',
-        active: true,
-      },
-      {
-        sno: 1,
-        title: 'original title',
-        desc: 'description always',
-        active: false,
-      },
-    ];
+    this.localItem = localStorage.getItem('todos');
+    if (this.localItem == null) this.todos = [];
+    else {
+      this.todos = JSON.parse(this.localItem);
+    }
   }
 
   ngOnInit(): void {}
@@ -39,10 +26,13 @@ export class ToDosComponent implements OnInit {
     let index = this.todos.indexOf(todo);
 
     this.todos.splice(index, 1);
+
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
   addTodo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
