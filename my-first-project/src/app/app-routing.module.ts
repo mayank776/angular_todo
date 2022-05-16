7c8db36102f82b6ae3b74f60b18f71de343bd0f2
Mayank Attri <attrimayank16@gmail.com>
2022-05-17 03:28:03 +0530

    final push

--- my-first-project/src/app/app-routing.module.ts
+++ my-first-project/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'a',
+    loadChildren: () =>
+      import('./modulea/modulea.module').then((m) => m.ModuleaModule),
+  },
+  {
+    path: 'b',
+    loadChildren: () =>
+      import('./moduleb/moduleb.module').then((m) => m.ModulebModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
